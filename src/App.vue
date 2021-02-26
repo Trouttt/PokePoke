7f1ad4391f8108ae18d3decaa582b20a167bd0eb
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-02-26 16:21:00 -0300

    Chore: added axios.get in pokemon.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div id="app">
-
+    <div v-for="(pokemon, index) in pokemons" :key="index">
+      <Pokemon :name="pokemon.name" :url="pokemon.url" :num="index + 1"/>
+    </div>
   </div>
 </template>
 
 <script>
+  import Pokemon from './components/Pokemon';
   import axios from 'axios';
   export default {
     name: 'App',
+    data(){
+      return{
+        pokemons: []
+      }
+    },
     created: function(){
-      console.log(axios);
-      console.log("teste");
+      axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+        
+        this.pokemons = res.data.results;
+        console.log(this.pokemons);
+      })
+    },
+    components:{
+      Pokemon
     }
   }
 </script>
